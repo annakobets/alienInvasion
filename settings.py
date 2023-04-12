3af32fcb20725f7caf08eeb48913591068262f21
Suruys <anna_kobets_20ist1@mail.ru>
2023-04-12 23:46:38 +0300

    piloting the ship

--- settings.py
+++ settings.py
@@ -1,6 +1,13 @@
+import pygame.display
 
-class Settings():
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
     def __init__(self):
-        self.screenWidth = 1000
-        self.screenHeight = 600
-        self.backColor = (230, 230, 230)
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
+        self.ship_speed = 1.5
